0fd9fe6e505d8320aa76baa4094d60013c094b50
aliceinthyme <alice.neuhaus@protonmail.com>
2025-12-09 13:25:19 +0100

    Update matricks.cpp

--- matricks.cpp
+++ matricks.cpp
@@ -1,6 +1,14 @@
+/*
+ * matricks.cpp
+ *
+ *  Created on: Dec 9, 2025
+ *      Author: $7ik000-l759v00niuic
+ */
+
 module;
 
 import <vector>;
+import <iostream>;
 
 export module matricks;
 
@@ -13,17 +21,48 @@ public:
 	explicit matricks(int sizeX = 4, int sizeY = 4, double init = 0.0);
 	matricks operator+=(const matricks& a);
 	matricks operator+=(double a);
+	matricks operator+(const matricks &a) const;
+
 	matricks operator-=(const matricks& a);
 	matricks operator-=(double a);
-	friend matricks operator+(const matricks& a, const matricks& b);
-	friend matricks operator+(double a, const matricks& b);
-	friend matricks operator+(const matricks& a, double b);
-	friend matricks operator-(const matricks& a, const matricks& b);
-	friend matricks operator-(double a, const matricks& b);
-	friend matricks operator-(const matricks& a, double b);
+	matricks operator-(const matricks &a) const;
+
 	matricks operator-();
-	matricks operator[](int i, int j);
-	matricks operator<<(const matricks& a);
-	matricks operator>>(const matricks& a);
+
+	std::vector<double>& operator[](int i);
+	const std::vector<double>& operator[](int i) const;
 };
+
+export std::ostream& operator<<(std::ostream& os, const matricks& mat);
+export std::istream& operator>>(std::istream& is, matricks& mat);
+
+export matricks operator+(double a, const matricks& b);
+export matricks operator+(const matricks& a, double b);
+export matricks operator-(double a, const matricks& b);
+export matricks operator-(const matricks& a, double b);
+
+
+void matricks::resize(int sizeX, int sizeY, double init) {		//compiler says its okay
+	a.resize(sizeX);
+	for(auto &i : a) {
+		i.resize(sizeY, init);
+	}
+}
+matricks::matricks(int sizeX, int sizeY, double init) {
+	this->resize(sizeX, sizeY, init);
+}
+
+std::vector<double>& matricks::operator[](int i) {
+	return a[i];
+}
+
+const std::vector<double>& matricks::operator[](int i) const {
+	return a[i];
+}
+
+
+
+
+
+
 
